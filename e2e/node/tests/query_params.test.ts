333b25bbc71153df61bf12316aefafc8b353d1c8
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-11-03 17:36:50 +0100

    feat: add ts

--- e2e/node/tests/query_params.test.ts
+++ e2e/node/tests/query_params.test.ts
@@ -518,7 +518,7 @@ describe("query_params", () => {
 		const app = createAppQueryParamsMultipleQueryParametersWithDifferentTypes();
 		const client = new TestClient(app);
 
-		const response = await client.get("/query/multi-type?age=30&name=john&active=true&score=95.5");
+		const response = await client.get("/query/multi-type?name=john&active=true&score=95.5&age=30");
 
 		expect(response.statusCode).toBe(200);
 		const responseData = response.json();
