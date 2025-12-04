ded2472424fd70764ff7207c1c199c16e2ca6757
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-04 16:03:24 +0100

    fix: unblock windows ruby and adjust coverage scope

---
+++ packages/ruby/ext/spikard_rb/native/include/msvc_compat/unistd.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef _MSC_VER
+#include <io.h>
+#include <process.h>
+#include <direct.h>
+
+typedef long ssize_t;
+
+#define access _access
+#define dup2 _dup2
+#define execve _execve
+#define ftruncate _chsize
+#define unlink _unlink
+#define getpid _getpid
+#define isatty _isatty
+#define lseek _lseek
+#define read _read
+#define write _write
+#define close _close
+#define sleep _sleep
+
+#else
+#include_next <unistd.h>
+#endif
