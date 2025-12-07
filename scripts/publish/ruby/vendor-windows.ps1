b52a8b3926ce22edd2c382d1668127e9753a1db8
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-07 09:44:30 +0100

    fix: resolve publish workflow script failures

--- scripts/publish/ruby/vendor-windows.ps1
+++ scripts/publish/ruby/vendor-windows.ps1
@@ -3,4 +3,4 @@ Set-StrictMode -Version Latest
 
 $workspace = ridk exec bash -lc "cygpath -au '$env:GITHUB_WORKSPACE'"
 $gemdir = "$workspace/packages/ruby"
-ridk exec bash -lc "cd $gemdir && bundle exec rake vendor:sync"
+ridk exec bash -lc "cd $gemdir && bundle exec rake clean && bundle exec rake build"
