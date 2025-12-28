1a1c2cd1e4b6de6078e466be41bfb096b72fef1f
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-28 18:52:37 +0100

    fix(ruby): remove --locked flag for release builds
    The --locked flag was preventing Cargo.lock updates in CI during release
    builds. Now only development builds use --locked, release builds can update
    the lock file after version bumps.

--- packages/ruby/ext/spikard_rb/extconf.rb
+++ packages/ruby/ext/spikard_rb/extconf.rb
@@ -7,7 +7,7 @@ default_profile = ENV.fetch('CARGO_PROFILE', 'release')
 
 create_rust_makefile('spikard_rb') do |config|
   config.profile = default_profile.to_sym
-  # Use --locked to prevent Cargo from updating the committed lockfile
-  # This avoids lockfile collision errors with vendored crates in CI
-  config.extra_cargo_args = ['--locked']
+  # Only use --locked in development to prevent lockfile updates
+  # Release builds need to update Cargo.lock after version bumps
+  config.extra_cargo_args = ['--locked'] unless default_profile == 'release'
 end
