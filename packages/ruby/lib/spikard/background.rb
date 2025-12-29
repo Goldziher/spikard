6da81eeb767bc9cce3bf2cac2e89988a7240fb7c
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2025-12-29 14:46:31 +0100

    Shutdown Ruby background worker after specs

--- packages/ruby/lib/spikard/background.rb
+++ packages/ruby/lib/spikard/background.rb
@@ -6,13 +6,27 @@ module Spikard
     module_function
 
     @queue = Queue.new
-    @worker = Thread.new do
-      loop do
-        job = @queue.pop
-        begin
-          job.call
-        rescue StandardError => e
-          warn("[spikard.background] job failed: #{e.message}")
+    @worker = nil
+    @worker_mutex = Mutex.new
+    SHUTDOWN = Object.new
+
+    def ensure_worker
+      return if @worker&.alive?
+
+      @worker_mutex.synchronize do
+        return if @worker&.alive?
+
+        @worker = Thread.new do
+          loop do
+            job = @queue.pop
+            break if job.equal?(SHUTDOWN)
+
+            begin
+              job.call
+            rescue StandardError => e
+              warn("[spikard.background] job failed: #{e.message}")
+            end
+          end
         end
       end
     end
@@ -21,7 +35,20 @@ module Spikard
     def run(&block)
       raise ArgumentError, 'background.run requires a block' unless block
 
+      ensure_worker
       @queue << block
+    end
+
+    # Stop the background worker thread to allow process shutdown.
+    def shutdown
+      @worker_mutex.synchronize do
+        return unless @worker&.alive?
+
+        @queue << SHUTDOWN
+        @worker.join(1)
+        @worker.kill if @worker.alive?
+        @worker = nil
+      end
     end
   end
 end
